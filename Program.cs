1da31e3c0dc981103bd08be194c2ac3cb0418273
Amit Kanti Barua <amitkanti.barua@petronas.com>
2023-11-13 15:39:02 +0600

    First commit on 13/11/2023

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace RefvsOut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int newVal1 = 20;
+            ValClass.AddNo(newVal1);
+            Console.WriteLine(newVal1);
+
+            int newVal2 = 20;
+            RefClass.AddNoWithRef(ref newVal2);
+            Console.WriteLine(newVal2);
+        }
+    }
+
+    public static class ValClass
+    {
+        public static int AddNo(int val)
+        {
+            val = val + 20;
+
+            return val;
+        }
+    }
+
+    public static class RefClass
+    {
+        public static int AddNoWithRef(ref int val)
+        {
+            val = val + 20; //With ref and out, actual value isn't passed to the
+                                         //method, instead a memory address pointer is referenced to keep track of changes
+
+            return val;
+        }
+
+        public static int AddNoWithOut(out int val)
+        {
+            val = 0; //Here the parameter val has to be initialized
+            val = val + 20;
+
+            return val;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+﻿# Project Objective
+
+This project was built with .NET Core 5 and the purpose of this mini project is to understand the difference between ref and out keyword.
+
+## ref & out
+
+Both are examples of call by reference.
+
+To know more about these two keywords, please visit this link - [Click Here](https://t.ly/M2nIu)
